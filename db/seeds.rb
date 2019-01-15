15f8b242c9669c819c5a813c04530416dafddd83
Matt The Wizard <matthewberger@finsync.com>
2019-01-15 13:09:29 -0500

    Setting up heroku environment

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+coach = User.create!( username: 'james-clay',
+                      password: 'supersecret',
+                      password_confirmation: 'supersecret',
+                      email: 'jamesclayfitness@gmail.com',
+                      name: 'James Clay')
+
+Client.create!(name: 'Ruth Berger', coach: coach)
+Client.create!(name: 'Matthew Berger', coach: coach)
