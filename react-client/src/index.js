4b37b84eb8fb580dcee5e472c6b6450b1cf77fb1
Andrew Smith <andrew@andrew.codes>
2019-01-18 12:59:29 -0500

    include redux devtools in non-production environments

--- react-client/src/index.js
+++ react-client/src/index.js
@@ -2,36 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './containers/App';
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware, compose } from './redux';
 import thunk from 'redux-thunk';
 import Reducer from './reducers';
 import * as serviceWorker from './serviceWorker';
 
 const defaultState = {
-	clients: {},
-	clientsOrder: [],
-	errorMessage: '',
-	username: '',
-	password: '',
-	token: ''
+  clients: {},
+  clientsOrder: [],
+  errorMessage: '',
+  username: '',
+  password: '',
+  token: '',
 };
 
-const composeEnhancers = compose;
 const store = createStore(
-	Reducer,
-	defaultState,
-	composeEnhancers(
-		applyMiddleware(thunk),
-	)
+  Reducer,
+  defaultState,
+  compose(applyMiddleware(thunk)),
 );
 
 const render = Component => {
-	ReactDOM.render(
-		<Provider store={store}>
-			<Component />
-		</Provider>,
-		document.getElementById('root')
-	);
+  ReactDOM.render(
+    <Provider store={store}>
+      <Component />
+    </Provider>,
+    document.getElementById('root'),
+  );
 };
 
 render(App);
